0776d34b627e5d662c872dd393d1d861c1405aac
xImAnton <anton.vogelsang@outlook.de>
2021-07-05 09:35:57 +0200

    made the address of mongo configurable through docker secrets

--- semoxy/io/mongo.py
+++ semoxy/io/mongo.py
@@ -9,7 +9,10 @@ class MongoClient(motor.motor_asyncio.AsyncIOMotorClient):
     def __init__(self, mc, loop):
         username = urllib.parse.quote_plus(Config.get_docker_secret("mongo_user") or Config.MONGO['username'])
         password = urllib.parse.quote_plus(Config.get_docker_secret("mongo_password") or Config.MONGO['password'])
-        uri = f"mongodb://{username}:{password}@{Config.MONGO['host']}:{Config.MONGO['port']}/mcweb?authSource=admin"
+        mongo_address: str = Config.get_docker_secret("mongo_address") or f"{Config.MONGO['host']}:{Config.MONGO['port']}"
+
+        uri = f"mongodb://{username}:{password}@{mongo_address}/semoxy?authSource=admin"
+
         super(MongoClient, self).__init__(uri, io_loop=loop)
         self.mc = mc
         self.db = self[Config.MONGO["database"]]
